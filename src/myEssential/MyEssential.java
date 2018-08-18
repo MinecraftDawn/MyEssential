21b9305d6177c56309009656859dc537815224d7
MinecraftDawn <eric4902077@gmail.com>
2018-08-18 19:45:01 +0800

    F

--- src/myEssential/MyEssential.java
+++ src/myEssential/MyEssential.java
@@ -1,13 +1,33 @@
 package myEssential;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
 import myEssential.commands.cmdManager;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MyEssential extends JavaPlugin {
+
+    private ProtocolManager protocolManager;
+
     @Override
     public void onEnable() {
         Bukkit.getPluginCommand("tp").setExecutor((CommandExecutor) new cmdManager());
+
+        protocolManager = ProtocolLibrary.getProtocolManager();
+
+        protocolManager.addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Server.TAB_COMPLETE) {
+            @Override
+            public void onPacketSending(PacketEvent e) {
+                if (e.getPacketType() == PacketType.Play.Server.TAB_COMPLETE)
+                    e.setCancelled(true);
+            }
+        });
     }
+
 }
